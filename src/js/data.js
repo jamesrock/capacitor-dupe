12c2f192f0b306ac43e3ed075ee81598a72011fd
James Rock <hello@jamesrock.me>
2025-04-04 11:04:53 +0100

    updates

--- src/js/data.js
+++ src/js/data.js
@@ -129,7 +129,7 @@ export const data = {
 				'8',
 				'9'
 			].flatMap((char) => {
-				return [[char, 1], [char, 2], [char, 3]];
+				return shuffle([[char, 1], [char, 2], [char, 3]]);
 			}))
 		}
 	},
