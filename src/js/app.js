b2f828864422d852a329a68e7d07618804ba6f23
James Rock <hello@jamesrock.me>
2025-04-04 10:26:21 +0100

    updates

--- src/js/app.js
+++ src/js/app.js
@@ -6,40 +6,17 @@ symbols = 'letters',
 cards = 'order11',
 rotations = [0, 90, 270];
 
-let targetNodes = null;
+let 
+selected = [],
+active = null;
 
 const getRandomRotation = () => rotations[Math.floor(Math.random() * rotations.length)];
 const getRandomBlockRotation = () => Math.floor(Math.random() * 360);
 const getRandomCard = () => shuffle(data.cards[cards][Math.floor(Math.random() * data.cards[cards].length)]);
 const getRandomSymbolIndex = () => Math.floor(Math.random() * data.symbols[symbols][cards].length);
-const highlightRandomBlocks = () => {
-
-	// const blocks = shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8]);
-	// const spliceCount = Math.floor(Math.random() * (2 - 1 + 1) + 1);
-	const blocksToHighlight = [0];
-	console.log(blocksToHighlight);
-
-	targetNodes = blocksToHighlight.map((id) => {
-		return document.querySelector(`[data-id="${id}"]`);
-	});
-
-	console.log(targetNodes);
-	
-	// document.querySelectorAll(`.container`).forEach((container) => {
-	// 	container.classList.remove('focus');
-	// });
-
-	// targetNodes.forEach((node) => {
-	// 	node.classList.add('focus');
-	// });
-
-	document.querySelectorAll(`.blocks.completed`).forEach((blocksNode) => {
-		blocksNode.parentNode.removeChild(blocksNode);
-	});
-
-};
 
 const gridNode = createNode('div', 'grid');
+const container = createNode('div', 'container');
 
 const createBlock = () => {
 
@@ -62,9 +39,9 @@ const createBlock = () => {
 	letters.forEach((letter, i) => {
 		const blockNode = createNode('div', 'block');
 		const rotateNode = createNode('span', 'block-value');
-		blockNode.setAttribute('data-value', letter[0]);
-		blockNode.setAttribute('data-shape', letter[1]);
-		blockNode.setAttribute('data-index', i);
+		blockNode.setAttribute('data-value', `${letter[0]}${letter[1]}`);
+		blockNode.setAttribute('data-color', letter[1]);
+		// blockNode.setAttribute('data-index', i);
 		rotateNode.innerHTML = letter[0];
 		rotateNode.style.transform = `rotate(${getRandomBlockRotation()}deg)`;
 		blockNode.append(rotateNode);
@@ -79,8 +56,12 @@ const createBlock = () => {
 
 };
 
-let selected;
-let count = 0;
+const deselect = () => {
+	selected.forEach((item) => {
+		item.classList.remove('selected');
+	});
+	selected = [];
+};
 
 gridNode.addEventListener('click', (e) => {
 	
@@ -90,69 +71,58 @@ gridNode.addEventListener('click', (e) => {
 		return;
 	};
 	
-	if(selected) {
-		if(selected === e.target) {
-			// unselect
-			selected.classList.remove('selected');
-			selected = null;
-			count --;
-		}
-		else if(e.target.getAttribute('data-value') === selected.getAttribute('data-value')) {
-
-			count ++;
-			
-			e.target.classList.add('selected');
-			// selected.classList.add('matched');
-			
-			if(count === targetNodes.length || targetNodes.length === 1 && count === 2) {
-
-				targetNodes.forEach((node) => {
-					node.prepend(createBlock());
-				});
-
-				setTimeout(() => {
-
-					targetNodes.forEach((node) => {
-						node.querySelector('.blocks:last-child').classList.add('completed');
-					});
-
-					setTimeout(() => {
-						highlightRandomBlocks();
-					}, 250);
-
-				}, 500);
-
-				selected = null;
-				count = 0;
-
-			};
-
-		};
+	if(selected.includes(e.target)) {
+		e.target.classList.remove('selected');
+		selected.splice(selected.indexOf(e.target, 1));
 	}
 	else {
-		selected = e.target;
-		selected.classList.add('selected');
-		count ++;
+		selected.push(e.target);
+		e.target.classList.add('selected');
 	};
 
-	console.log('count', count);
+	if(selected.length === 2) {
+
+		if(selected[0].getAttribute('data-value') === selected[1].getAttribute('data-value')) {
+
+			const newBlock = createBlock();
+			
+			container.prepend(newBlock);
+
+			setTimeout(() => {
+
+				active.classList.add('completed');
+
+				setTimeout(() => {
+					active.parentNode.removeChild(active);
+					active = newBlock;
+				}, 250);
+
+			}, 500);
+
+			selected = [];
+
+		}
+		else {
+			
+			deselect();
+
+		};
+
+	};
 	
 });
 
-console.log('cards', data.cards[cards]);
-console.log('symbols', data.symbols[symbols][cards]);
+document.addEventListener('click', (e) => {
+	if(!e.target.getAttribute('data-value')) {
+		deselect();
+	};
+});
 
-const levels = 1;
-
-for(var i=0;i<levels;i++) {
-	const container = createNode('div', 'container');
-	container.setAttribute('data-id', i);
-	container.append(createBlock());
-	gridNode.append(container);
-};
+active = createBlock();
+container.append(active);
+gridNode.append(container);
 
 document.body.append(gridNode);
 
-highlightRandomBlocks();
-
-console.log(data.symbols[symbols][cards]);
+// console.log('cards', data.cards[cards]);
+// console.log('symbols', data.symbols[symbols][cards]);
